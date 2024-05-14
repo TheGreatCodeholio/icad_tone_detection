0f7b8fb5ed687d9300c1ab4f256a224cb65bf0e5
thegreatcodeholio <ian@icarey.net>
2024-05-14 14:07:27 -0400

    added examples. updated build bumped to version 1.0

--- examples/detect_test.py
+++ examples/detect_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json
 import sys
-from src.icad_tone_detection import tone_detect
+from icad_tone_detection import tone_detect
 
 if len(sys.argv) > 1:
     audio_path = sys.argv[1]
@@ -10,9 +10,6 @@ else:
     exit(0)
 
 detect_result = tone_detect(audio_path, matching_threshold=1.5, time_resolution_ms=50, debug=True)
-
-if len(detect_result.two_tone_result) == 0 and len(detect_result.long_result) == 0 and len(detect_result.hi_low_result) == 0:
-    print("No tones")
 
 data_dict = {"two_tone": detect_result.two_tone_result, "long_tone": detect_result.long_result, "hl_tone": detect_result.hi_low_result}
 
